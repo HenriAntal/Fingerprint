b4fa9b01d3b2fb4c7c25cc45adfe704b97080706
HenriAntal <93285838+HenriAntal@users.noreply.github.com>
2021-10-28 00:01:50 +0200

    oh it works yessss
    intellij yay

--- src/cs107/Fingerprint.java
+++ src/cs107/Fingerprint.java
@@ -7,7 +7,7 @@ import java.util.List;
 /**
  * Provides tools to compare fingerprint.
  */
-public class Fingerprint {
+public class Fingerprint { //test
 
   /**
    * The number of pixels to consider in each direction when doing the linear
@@ -68,7 +68,7 @@ public class Fingerprint {
   public static boolean[] getNeighbours(boolean[][] image, int row, int col) {
 	  assert (image != null); // special case that is not expected (the image is supposed to have been checked
                               // earlier)
-	  //TODO implement
+	  //TODO implement what is this?? hello
 	  return null;
   }
 
